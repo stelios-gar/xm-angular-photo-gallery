4feb7d7aa3afde7e2caf1e4d8dfe01561b5b8f6f
stelgar <stelgrfdev@gmail.com>
2026-05-03 11:13:56 +0300

    feat: app routing configuration

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PhotosPageComponent } from './features/photos/photos-page/photos-page.component';
+import { FavoritesPageComponent } from './features/favorites/favorites-page/favorites-page.component';
+import { PhotoDetailPageComponent } from './features/photo-detail/photo-detail-page/photo-detail-page.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', redirectTo: 'photos', pathMatch: 'full' },
+  { path: 'photos', component: PhotosPageComponent },
+  { path: 'favorites', component: FavoritesPageComponent },
+  { path: 'photos/:id', component: PhotoDetailPageComponent },
+  { path: '**', redirectTo: 'photos' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
