41ed38cbee63a0921b4344078478d6c04778a08a
stelgar <stelgrfdev@gmail.com>
2026-05-03 12:37:24 +0300

    feat: implement favorites page FavoritesPageComponent

--- src/app/features/favorites/favorites-page/favorites-page.component.ts
+++ src/app/features/favorites/favorites-page/favorites-page.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FavoritesService } from '../../../core/services/favorites.service';
+import { Photo } from '../../../shared/models/photo';
 
 @Component({
   selector: 'app-favorites-page',
   templateUrl: './favorites-page.component.html',
   styleUrls: ['./favorites-page.component.scss']
 })
-export class FavoritesPageComponent {
+export class FavoritesPageComponent implements OnInit {
+  favoritePhotos: Photo[] = [];
 
+  constructor(
+    private favoritesService: FavoritesService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.favoritePhotos = this.favoritesService.getFavorites();
+  }
+
+  openPhoto(photo: Photo): void {
+    this.router.navigate(['/photos', photo.id]);
+  }
 }
